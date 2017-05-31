88d7fb590943302c7e50ff1333dac002c714754a
Felipe Marinho <felipmarinho@gmail.com>
2017-05-31 17:33:22 -0300

    Update FiltraNegociacoesIT.java
    Adicionei o parametro de local para do local do webdrive na minha maquina.

--- src/test/java/br/com/caelum/argentum/bean/FiltraNegociacoesIT.java
+++ src/test/java/br/com/caelum/argentum/bean/FiltraNegociacoesIT.java
@@ -62,7 +62,8 @@ public class FiltraNegociacoesIT {
 			
 			iniciaPhantomJs();
 			
-		} else if (System.getProperty("webdriver.chrome.driver") != null) {
+		} else if (System.getProperty("webdriver.chrome.driver","/home/felipe/Documentos/Estudos/Java/DevOps/Recursos
+/chromedriver") != null) {
 			
 			iniciaChrome();
 			
